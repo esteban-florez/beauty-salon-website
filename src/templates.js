ed2ec236a91f0d03d6841d2dde1f4da471cad788
esteban-florez <eflorez077@gmail.com>
2023-06-07 18:13:21 -0400

    fixed loading svg

--- src/templates.js
+++ src/templates.js
@@ -9,7 +9,12 @@ export const errorModal = `
 `
 
 export const loadingModal = `
-  <img class="w-28 h-28 animate-spin" src="/icons/loading.svg">
+  <svg class="w-28 h-28 animate-spin" width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none">
+    <g fill="#000000" fill-rule="evenodd" clip-rule="evenodd">
+      <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"/>
+      <path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"/>
+    </g>
+  </svg>
   <p class="font-semibold text-2xl mt-4">Se está enviando su opinión...</p>
 `
 
